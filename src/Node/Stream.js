eb7ed41b5a21630dd380806313de0ffd6e933eb2
Gary Burgess <gary.burgess@gmail.com>
2016-10-17 22:47:09 +0100

    Update build

--- src/Node/Stream.js
+++ src/Node/Stream.js
@@ -1,173 +1,171 @@
-/* global exports */
-/* global Buffer */
 "use strict";
 
 exports.undefined = undefined;
 
-exports.setEncodingImpl = function(s) {
-    return function(enc) {
-        return function() {
-            s.setEncoding(enc);
-        };
+exports.setEncodingImpl = function (s) {
+  return function (enc) {
+    return function () {
+      s.setEncoding(enc);
     };
+  };
 };
 
-exports.readChunkImpl = function(Left) {
-    return function(Right) {
-        return function(chunk) {
-            if (chunk instanceof Buffer) {
-                return Right(chunk);
-            } else if (typeof chunk === 'string') {
-                return Left(chunk);
+exports.readChunkImpl = function (Left) {
+  return function (Right) {
+    return function (chunk) {
+      if (chunk instanceof Buffer) {
+        return Right(chunk);
+      } else if (typeof chunk === "string") {
+        return Left(chunk);
+      } else {
+        throw new Error(
+          "Node.Stream.readChunkImpl: Unrecognised " +
+          "chunk type; expected String or Buffer, got: " +
+          chunk);
+      }
+    };
+  };
+};
+
+exports.onDataEitherImpl = function (readChunk) {
+  return function (r) {
+    return function (f) {
+      return function () {
+        r.on("data", function (data) {
+          f(readChunk(data))();
+        });
+      };
+    };
+  };
+};
+
+exports.onEnd = function (s) {
+  return function (f) {
+    return function () {
+      s.on("end", f);
+    };
+  };
+};
+
+exports.onReadable = function (s) {
+  return function (f) {
+    return function () {
+      s.on("readable", f);
+    };
+  };
+};
+
+exports.onError = function (s) {
+  return function (f) {
+    return function () {
+      s.on("error", function (e) {
+        f(e)();
+      });
+    };
+  };
+};
+
+exports.onClose = function (s) {
+  return function (f) {
+    return function () {
+      s.on("close", f);
+    };
+  };
+};
+
+exports.resume = function (s) {
+  return function () {
+    s.resume();
+  };
+};
+
+exports.pause = function (s) {
+  return function () {
+    s.pause();
+  };
+};
+
+exports.isPaused = function (s) {
+  return function () {
+    return s.isPaused();
+  };
+};
+
+exports.pipe = function (r) {
+  return function (w) {
+    return function () {
+      return r.pipe(w);
+    };
+  };
+};
+
+exports.readImpl = function (readChunk) {
+  return function (Nothing) {
+    return function (Just) {
+      return function (r) {
+        return function (s) {
+          return function () {
+            var v = r.read(s);
+            if (v === null) {
+              return Nothing;
             } else {
-                throw new Error(
-                  "Node.Stream.readChunkImpl: Unrecognised " +
-                  "chunk type; expected String or Buffer, got: " +
-                  chunk);
+              return Just(readChunk(v));
             }
+          };
         };
+      };
     };
+  };
 };
 
-exports.onDataEitherImpl = function(readChunk) {
-    return function(r) {
-        return function(f) {
-            return function() {
-                r.on('data', function(data) {
-                    f(readChunk(data))();
-                });
-            };
+exports.write = function (w) {
+  return function (chunk) {
+    return function (done) {
+      return function () {
+        return w.write(chunk, null, done);
+      };
+    };
+  };
+};
+
+exports.writeStringImpl = function (w) {
+  return function (enc) {
+    return function (s) {
+      return function (done) {
+        return function () {
+          return w.write(s, enc, done);
         };
+      };
     };
+  };
 };
 
-exports.onEnd = function(s) {
-    return function(f) {
-        return function() {
-            s.on('end', f);
-        };
-    };
+exports.cork = function (w) {
+  return function () {
+    return w.cork();
+  };
 };
 
-exports.onReadable = function(s) {
-    return function(f) {
-        return function() {
-            s.on('readable', f);
-        };
-    };
+exports.uncork = function (w) {
+  return function () {
+    return w.uncork();
+  };
 };
 
-exports.onError = function(s) {
-    return function(f) {
-        return function() {
-            s.on('error', function(e) {
-                f(e)();
-            });
-        };
+exports.setDefaultEncodingImpl = function (w) {
+  return function (enc) {
+    return function () {
+      w.setDefaultEncoding(enc);
     };
+  };
 };
 
-exports.onClose = function(s) {
-    return function(f) {
-        return function() {
-            s.on('close', f);
-        };
+exports.end = function (w) {
+  return function (done) {
+    return function () {
+      w.end(null, null, function () {
+        done();
+      });
     };
-};
-
-exports.resume = function(s) {
-    return function() {
-        s.resume();
-    };
-};
-
-exports.pause = function(s) {
-    return function() {
-        s.pause();
-    };
-};
-
-exports.isPaused = function(s) {
-    return function() {
-        return s.isPaused();
-    };
-};
-
-exports.pipe = function(r) {
-    return function(w) {
-        return function() {
-            return r.pipe(w);
-        };
-    };
-};
-
-exports.readImpl = function(readChunk) {
-    return function(Nothing) {
-        return function(Just) {
-            return function(r) {
-                return function(s) {
-                    return function() {
-                        var v = r.read(s);
-                        if (v === null) {
-                            return Nothing;
-                        } else {
-                            return Just(readChunk(v));
-                        }
-                    };
-                };
-            };
-        };
-    };
-};
-
-exports.write = function(w) {
-    return function(chunk) {
-        return function(done) {
-            return function() {
-                return w.write(chunk, null, done);
-            };
-        };
-    };
-};
-
-exports.writeStringImpl = function(w) {
-    return function(enc) {
-        return function(s) {
-            return function(done) {
-                return function() {
-                    return w.write(s, enc, done);
-                };
-            };
-        };
-    };
-};
-
-exports.cork = function(w) {
-    return function() {
-        return w.cork();
-    };
-};
-
-exports.uncork = function(w) {
-    return function() {
-        return w.uncork();
-    };
-};
-
-exports.setDefaultEncodingImpl = function(w) {
-    return function(enc) {
-        return function() {
-            w.setDefaultEncoding(enc);
-        };
-    };
-};
-
-exports.end = function(w) {
-    return function(done) {
-        return function() {
-            w.end(null, null, function() {
-                done();
-            });
-        };
-    };
+  };
 };
