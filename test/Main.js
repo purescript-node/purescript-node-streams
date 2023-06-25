45d20da686e0d9e3890783c4f143d6f2640c67fb
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-06-24 21:31:54 -0500

    Update tests

--- test/Main.js
+++ test/Main.js
@@ -1,25 +1,5 @@
-import { WritableStreamBuffer, ReadableStreamBuffer } from "stream-buffers";
-import { PassThrough } from "stream";
-
-export function writableStreamBuffer() {
-  return new WritableStreamBuffer;
-}
-
-export function getContentsAsString(w) {
-  return () => w.getContentsAsString("utf8");
-}
-
-export function readableStreamBuffer() {
-  return new ReadableStreamBuffer;
-}
-
-export function putImpl(str) {
-  return enc => r => () => {
-    r.put(str, enc);
-  };
-}
-
 export { createGzip, createGunzip } from "zlib";
+import { PassThrough } from "stream";
 
 export function passThrough() {
   return new PassThrough;
