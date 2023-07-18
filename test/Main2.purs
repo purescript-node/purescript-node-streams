639c74a0dc5d3ea7b331bd1c4850722196edce21
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-18 14:57:48 -0700

    Refactor: verify line count test is correct

--- test/Main2.purs
+++ test/Main2.purs
@@ -1,7 +1,7 @@
 -- | How to test:
 -- |
 -- | ```
--- | pulp test --main Test.Main2 | wc -c
+-- | pulp test --main Test.Main2
 -- | ```
 module Test.Main2 where
 
@@ -9,28 +9,32 @@ import Prelude
 
 import Data.Array as Array
 import Data.Either (Either(..))
+import Data.String (Pattern(..))
+import Data.String as String
 import Effect (Effect)
-import Effect.Aff (Error, runAff_)
+import Effect.Aff (Error, error, runAff_, throwError)
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
 import Node.Buffer as Buffer
 import Node.Encoding (Encoding(..))
-import Node.Stream (Writable)
-import Node.Stream.Aff (write)
-import Partial.Unsafe (unsafePartial)
+import Node.Stream (newPassThrough)
+import Node.Stream.Aff (readableToStringUtf8, write)
 import Unsafe.Coerce (unsafeCoerce)
 
-foreign import stdout :: Writable ()
-
-completion :: Either Error (Effect Unit) -> Effect Unit
+completion :: Either Error Unit -> Effect Unit
 completion = case _ of
   Left e -> Console.error (unsafeCoerce e)
-  Right f -> f
+  Right _ -> mempty
 
 main :: Effect Unit
-main = unsafePartial $ do
+main = do
+  duplex <- newPassThrough
   runAff_ completion do
-    do
-      b <- liftEffect $ Buffer.fromString "aaaaaaaaaa" UTF8
-      write stdout $ Array.replicate 100000 b
-    pure (pure unit)
+    let expected = 100_000
+    b <- liftEffect $ Buffer.fromString "aaaaaaaaaa" UTF8
+    write duplex $ Array.replicate 100000 b
+    str <- readableToStringUtf8 duplex
+    let actual = Array.length (String.split (Pattern "\n") str)
+    unless (expected == expected) do
+      throwError $ error $ "Expected " <> show expected <> " lines, but got " <> show actual <> " lines."
+
